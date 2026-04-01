18b827899b2c938fb0085460d069e0d2bdc11947
Phil Timms <p.timms1@hotmail.co.uk>
2026-04-01 07:15:36 +0100

    Add Google Tag Manager

---
+++ .history/app/layout_20260401071419.tsx
@@ -0,0 +1,76 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Script from "next/script";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Timms Travel - Improve your trip with experiences, hotels, and more",
+  description:
+    "Choose from thousands of experiences, compare hotel prices, and reserve your hire car before your trip begins.",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K4RWSHXR');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        {/* GetYourGuide Analytics */}
+        <Script
+          src="https://widget.getyourguide.com/dist/pa.umd.production.min.js"
+          data-gyg-partner-id="P7B7GRH"
+          strategy="afterInteractive"
+          async
+          defer
+        />
+
+        {/* GetYourGuide Widget Engine */}
+        <Script
+          src="https://widget.getyourguide.com/dist/gyg-widget.js"
+          strategy="afterInteractive"
+          async
+        />
+      </head>
+
+      <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K4RWSHXR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        {children}
+      </body>
+    </html>
+  );
+}
