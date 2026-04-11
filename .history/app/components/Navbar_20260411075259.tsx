8d820a3018987ea31700467207eb35da9a48237b
Phil Timms <p.timms1@hotmail.co.uk>
2026-04-11 07:56:11 +0100

    Sort Mobile Navbar

--- .history/app/components/Navbar_20260411075259.tsx
+++ .history/app/components/Navbar_20260411075259.tsx
@@ -86,46 +86,49 @@ export default function Navbar() {
         </button>
       </div>
 
-{/* Mobile Menu */}
-{menuOpen ? (
-  <div
-    className="md:hidden mt-4 flex flex-col gap-4 pb-4 border-t pt-4"
-    style={{ borderColor: '#e5e7eb' }}
-  >
-    <a
-      href="/locations"
-      style={{ color: '#232e4e' }}
-      className="font-medium hover:opacity-75 transition"
-    >
-      Locations
-    </a>
-
-    <a
-      href="/flights"
-      style={{ color: '#232e4e' }}
-      className="font-medium hover:opacity-75 transition"
-    >
-      Flights
-    </a>
-
-    <a
-      href="/blog"
-      style={{ color: '#232e4e' }}
-      className="font-medium hover:opacity-75 transition"
-    >
-      Blog
-    </a>
-
-    <a
-      href="/about"
-      style={{ color: '#232e4e' }}
-      className="font-medium hover:opacity-75 transition"
-    >
-      About
-    </a>
-  </div>
-) : null}
-
+      {/* Mobile Menu */}
+      {menuOpen ? (
+        <div
+          className="md:hidden mt-4 flex flex-col gap-4 pb-4 border-t pt-4"
+          style={{ borderColor: '#e5e7eb' }}
+        >
+          <a
+            href="/locations"
+            style={{ color: '#232e4e' }}
+            className="font-medium hover:opacity-75 transition"
+          >
+            Locations
+            <
+            href="/flights"
+            style={{ color: '#232e4e' }}
+            className="font-medium hover:opacity-75 transition"
+          >
+            Flights
+          </a>
+          </a>
+          <a
+            href="/blog"
+            style={{ color: '#232e4e' }}
+            className="font-medium hover:opacity-75 transition"
+          >
+            Blog
+          </a>
+          <a
+            href="/about"
+            style={{ color: '#232e4e' }}
+            className="font-medium hover:opacity-75 transition"
+          >
+            About
+          </a>
+          <a
+            href="/contact"
+            style={{ color: '#232e4e' }}
+            className="font-medium hover:opacity-75 transition"
+          >
+            Contact Us
+          </a>
+        </div>
+      ) : null}
     </nav>
   )
 }
